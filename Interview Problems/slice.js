b094527411b98decac5862e9422b94dc40436eac
mdislammazharul <mazharul.py@gmail.com>
2021-09-16 17:51:04 +0600

    slice

---
+++ Interview Problems/slice.js
@@ -0,0 +1,4 @@
+let arrayIntegers = [1, 2, 3, 4, 5];
+let arrayIntegers1 = arrayIntegers.slice(0, 2); // returns 0 & 1 no index's element [1,2]
+let arrayIntegers2 = arrayIntegers.slice(2, 3); // returns 2 no index's element [3]
+let arrayIntegers3 = arrayIntegers.slice(4); //returns 4 no index's element [5]
